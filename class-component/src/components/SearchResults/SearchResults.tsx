df5474551af24222da4ae48919bc53c8032662da
Egor Cherkashin <egor9298@mail.ru>
2024-07-13 12:35:14 +0400

    refactor: Convert SearchResults component to functional component

--- class-component/src/components/SearchResults/SearchResults.tsx
+++ class-component/src/components/SearchResults/SearchResults.tsx
@@ -1,34 +1,28 @@
-import { Component } from 'react';
+import React from 'react';
 import styles from './SearchResults.module.css';
 import CharacterCard from '../Character/CharacterCard.tsx';
 import { SearchResultsProps } from '../../types.ts';
 
-class SearchResults extends Component<
-    SearchResultsProps,
-    Record<string, never>
-> {
-    render() {
-        const { results } = this.props;
+const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
 
-        // Искусственно вызываем ошибку для тестирования ErrorBoundary
-        // if (results.length > 0 && results[0].name === 'Luke Skywalker') {
-        //     throw new Error('Test error');
-        // }
+    // Искусственно вызываем ошибку для тестирования ErrorBoundary
+    // if (results.length > 0 && results[0].name === 'Luke Skywalker') {
+    //     throw new Error('Test error');
+    // }
 
-        return (
-            <div className={styles.container}>
-                {results.length > 0 ? (
-                    <div className={styles.grid}>
-                        {results.map((result, index) => (
-                            <CharacterCard key={index} character={result} />
-                        ))}
-                    </div>
-                ) : (
-                    <p>No results found</p>
-                )}
-            </div>
-        );
-    }
-}
+    return (
+        <div className={styles.container}>
+            {results.length > 0 ? (
+                <div className={styles.grid}>
+                    {results.map((result, index) => (
+                        <CharacterCard key={index} character={result} />
+                    ))}
+                </div>
+            ) : (
+                <p>No results found</p>
+            )}
+        </div>
+    );
+};
 
 export default SearchResults;
