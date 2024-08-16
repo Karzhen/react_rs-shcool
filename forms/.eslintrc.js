cd0dcbc77517a2f8d01787350e9c03b86df2c87e
Egor Cherkashin <egor9298@mail.ru>
2024-08-16 23:04:54 +0300

    init: Initialize project setup is completed

--- forms/.eslintrc.js
+++ forms/.eslintrc.js
@@ -1,12 +1,31 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
     extends: [
         'eslint:recommended',
+        'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
+        'prettier',
     ],
+    plugins: ['react', '@typescript-eslint'],
     rules: {
-        'no-undef': 'off',
         '@typescript-eslint/no-explicit-any': 'error',
+        'react/prop-types': 'off',
     },
 };
